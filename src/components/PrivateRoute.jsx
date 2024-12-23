ae99a7a3022ab39631a2a570c7826e56dfc9d920
Nayan Suter <nayankbps@gmail.com>
2024-12-23 18:45:44 +0600

    used private route

---
+++ src/components/PrivateRoute.jsx
@@ -0,0 +1,22 @@
+import { useContext } from "react";
+import { AuthContext } from "../provider/AuthProvider";
+import { Navigate } from "react-router-dom";
+
+const PrivateRoute = ({children}) => {
+    const {user}= useContext(AuthContext);
+
+    if(!user){
+        return( 
+            <Navigate to='/login'></Navigate>
+        )
+    }
+
+
+    return (
+        <div>
+            {children}
+        </div>
+    );
+};
+
+export default PrivateRoute;
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -9,22 +9,57 @@ import MyVisaApplications from "../pages/MyVisaApplications";
 import VisaDetails from "../pages/VisaDetails";
 import MyAddedVisa from "../pages/MyAddedVisa";
 import UpdateVisa from "../components/UpdateVisa";
+import PrivateRoute from "../components/PrivateRoute";
 
-const router= createBrowserRouter([
-    {path:'/',element:<HomeLayout/>, 
-        children:[
-            {path:'/', element:<Home/>},
-            {path:'/allVisas', element:<AllVisas/>, loader:()=>fetch('http://localhost:5000/allVisa')},
-            {path:'/addVisa', element:<AddVisa/>},
-            {path:'/myAdded/:email', element:<MyAddedVisa/>,
-                loader:({params})=>fetch(`http://localhost:5000/myAdded/${params.email}`)},
-            {path:'/visaDetails/:id', element:<VisaDetails/>,loader:({params})=>fetch(`http://localhost:5000/allVisa/${params.id}`)},
-            {path:'/updateVisa/:id', element:<UpdateVisa/>, loader: ({params})=>fetch(`http://localhost:5000/allVisa/${params.id}`)},
-           
-            {path:'/myApplications/:email', element:<MyVisaApplications/>, loader:({params})=>fetch(`http://localhost:5000/myApplied/${params.email}`)},
-            {path:'/register', element:<Registration/>},
-            {path:'/login', element:<Login/>},
-    ]}
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    children: [
+      { path: "/", element: <Home /> },
+      {
+        path: "/allVisas",
+        element: <AllVisas />,
+        loader: () => fetch("http://localhost:5000/allVisa"),
+      },
+      { path: "/addVisa", element: <AddVisa /> },
+      {
+        path: "/myAdded/:email",
+        element: <PrivateRoute>
+            <MyAddedVisa />
+        </PrivateRoute>,
+        loader: ({ params }) =>
+          fetch(`http://localhost:5000/myAdded/${params.email}`),
+      },
+      {
+        path: "/visaDetails/:id",
+        element: (
+          <PrivateRoute>
+            <VisaDetails />
+          </PrivateRoute>
+        ),
+        loader: ({ params }) =>
+          fetch(`http://localhost:5000/allVisa/${params.id}`),
+      },
+      {
+        path: "/updateVisa/:id",
+        element: <UpdateVisa />,
+        loader: ({ params }) =>
+          fetch(`http://localhost:5000/allVisa/${params.id}`),
+      },
+
+      {
+        path: "/myApplications/:email",
+        element: <PrivateRoute>
+            <MyVisaApplications />
+        </PrivateRoute>,
+        loader: ({ params }) =>
+          fetch(`http://localhost:5000/myApplied/${params.email}`),
+      },
+      { path: "/register", element: <Registration /> },
+      { path: "/login", element: <Login /> },
+    ],
+  },
 ]);
 
 export default router;
